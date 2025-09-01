4bacd9187d80c3a2fd043bdc8769bd754670d09f
Eden Reich <eden.reich@gmail.com>
2025-09-01 15:02:22 +0200

    fix: Improve error handling on readline closure in prompt functions
    Signed-off-by: Eden Reich <eden.reich@gmail.com>

--- internal/prompt/prompt.go
+++ internal/prompt/prompt.go
@@ -2,6 +2,7 @@ package prompt
 
 import (
 	"fmt"
+	"log"
 	"strings"
 
 	"github.com/chzyer/readline"
@@ -20,7 +21,11 @@ func ReadString(promptText, defaultValue string) (string, error) {
 	if err != nil {
 		return "", err
 	}
-	defer rl.Close()
+	defer func() {
+		if closeErr := rl.Close(); closeErr != nil {
+			log.Printf("failed to close readline: %v", closeErr)
+		}
+	}()
 
 	line, err := rl.Readline()
 	if err != nil {
@@ -45,7 +50,11 @@ func ReadPassword(promptText string) (string, error) {
 	if err != nil {
 		return "", err
 	}
-	defer rl.Close()
+	defer func() {
+		if closeErr := rl.Close(); closeErr != nil {
+			log.Printf("failed to close readline: %v", closeErr)
+		}
+	}()
 
 	password, err := rl.ReadPassword(fmt.Sprintf("%s: ", promptText))
 	if err != nil {
